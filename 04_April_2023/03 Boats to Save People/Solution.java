f487ae55455c932348e9c5e60b753aabbc527793
Alexandru Nastase <nastase.alexandru100@gmail.com>
2023-04-03 11:32:25 +0100

    03/04/2023

---
+++ 04_April_2023/03 Boats to Save People/Solution.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+//T: O(n log n) for sorting, O(n) afterwards
+//S: O(1)
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int l = 0;
+        int r = people.length - 1;
+        int boats = 0;
+
+        while (l <= r) {
+            if (people[l] + people[r] <= limit) {
+                l++;
+            }
+            r--;
+            boats++;
+        }
+
+        return boats;
+    }
+}

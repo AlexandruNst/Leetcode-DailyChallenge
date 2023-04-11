ec486d125c975b432d9f968de48c39e3934ce35e
Alexandru Nastase <nastase.alexandru100@gmail.com>
2023-04-11 17:34:09 +0100

    11/04/2023

---
+++ 04_April_2023/11 Removing Stars From a String/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+class Solution {
+    public String removeStars(String s) {
+        char[] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : chars) {
+            if (c != '*') {
+                stack.push(c);
+            } else {
+                stack.pop();
+            }
+        }
+
+        char[] res = new char[stack.size()];
+        int i = res.length - 1;
+        while (!stack.isEmpty()) {
+            res[i--] = stack.pop();
+        }
+
+        return new String(res);
+
+    }
+}

1b627a11143ca5256f837d0bfcb16cdfe2ea0907
Alexandru Nastase <nastase.alexandru100@gmail.com>
2023-05-18 10:57:12 +0100

    18/05/2023

---
+++ 05_May_2023/18 Minimum Number of Vertices to Reach All Nodes/Solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+        int[] indegree = new int[n];
+        for (List<Integer> edge : edges) {
+            int to = edge.get(1);
+            indegree[to]++;
+        }
+
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0)
+                res.add(i);
+        }
+
+        return res;
+    }
+}

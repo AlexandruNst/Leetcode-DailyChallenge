b361f8a45caadff1177d499e2bef39462fb7628c
Alexandru Nastase <nastase.alexandru100@gmail.com>
2023-11-06 20:16:44 +0000

    06/11/2023

---
+++ 11_November_2023/06 Seat Reservation Manager/Solution.java
@@ -0,0 +1,31 @@
+class SeatManager {
+    PriorityQueue<Integer> pq;
+    int seat;
+    int n;
+
+    public SeatManager(int n) {
+        pq = new PriorityQueue<>(n);
+        // for(int i = 1; i <= n; i++) pq.add(i);
+        seat = 1;
+        this.n = n;
+    }
+
+    public int reserve() {
+        // return pq.poll();
+        if (pq.size() > 0)
+            return pq.poll();
+        else
+            return seat++;
+    }
+
+    public void unreserve(int seatNumber) {
+        pq.add(seatNumber);
+    }
+}
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager obj = new SeatManager(n);
+ * int param_1 = obj.reserve();
+ * obj.unreserve(seatNumber);
+ */
